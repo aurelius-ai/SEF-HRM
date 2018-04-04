be14f40e1c02271cb56a264a4bde3c089ef92811
HunterTerence <36937301+HunterTerence@users.noreply.github.com>
2018-04-05 09:03:25 +1000

    Update SystemDriver.java

--- SystemDriver.java
+++ SystemDriver.java
@@ -1,24 +1,24 @@
+package HRSystem;
 import java.util.*;
-
 public class SystemDriver {
 	
 	public static void main(String args[])
 	{
-		int input;
-		boolean vaild;
+		String input;
+		boolean vaild=false;
 		Scanner sc = new Scanner(System.in);
 		do{
 		System.out.println("Do you have an account?\nIf you have, please enter 1 to login\nIf you don't have, please enter 2 to register.");
-		input=sc.nextInt();
-		if(input==1)
+		input=sc.nextLine();
+		if(input.equals("1"))
 		{
-    
 			vaild=true;
+			//go to login page/function str1.equals(str2)
 		}
-		else if(input==2)
+		else if(input.equals("2"))
 		{
-    
 			vaild=true;
+			//go to register page/function
 		}
 		else
 		{
