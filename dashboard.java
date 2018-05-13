d6a99d694406d99bd55196767955579e3904e3dc
HunterTerence <36937301+HunterTerence@users.noreply.github.com>
2018-05-13 22:26:37 +1000

    Create dashboard.java

---
+++ dashboard.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+public class dashboard extends JFrame{
+		public void dashboard1()
+		{
+			System.out.println("1.View/Edit profile\n2.Apply Course\n3.View salary\n");
+			
+		}
+		public void dashboard2()
+		{
+			System.out.println("1.Course corfirmation\n2.Responds of allocation\n");
+			
+			
+		}
+		public void dashboard3()
+		{
+
+			System.out.print("1.Design course\n2.Manage causal staff\n3.Sending allocation to approver");
+			
+		}
+		public void dashboard4()
+		{
+
+			System.out.print("1.update pay=rate table\n2.Generate report\n3.Add/Delete user");
+		}
+}
