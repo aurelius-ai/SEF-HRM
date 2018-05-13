18d9f169ca99821d496e5473266de3a501e8670b
ZhaonanTian <36937298+ZhaonanTian@users.noreply.github.com>
2018-05-13 22:02:54 +1000

    Add files via upload

---
+++ User.java
@@ -0,0 +1,24 @@
+public abstact class User
+{
+	protected String userName;
+	protected String password;
+	protected String name;
+	protected String role;
+
+	public User (String userName, String password, String name, String role)
+	{
+		this.userName = userName;
+		this.password = password;
+		this.name = name;
+		this.role = role;
+	}
+
+	public abstract String getUserName();
+	public abstract void setUserName(String userName);
+	public abstract String getPassword();
+	public abstract void setPassword(String Password);
+	public abstract String getName();
+	public abstract void setName(String name);
+	public abstract String getRole();
+
+}
